1ef158d1bf47ddbb753c27609b822b6f93313686
Thomas Vos <thomasvos@outlook.com>
2022-06-17 08:48:45 +0200

    commits juypter notebooks

--- downloadFunctions/moralis.py
+++ downloadFunctions/moralis.py
@@ -3,6 +3,9 @@ import requests
 from config import MORALISAPIKEY
 import time
 
+maxTries = 10
+downloadTime = 1/24
+
 def Moralis(limit=500, block_start=13296011, blocks=1, lines_to_save_data_per_block=50000, data_folder='', chain='eth'):
     my_headers = {'X-API-Key' : MORALISAPIKEY}
     df = pd.DataFrame({}, dtype=str)
@@ -13,21 +16,31 @@ def Moralis(limit=500, block_start=13296011, blocks=1, lines_to_save_data_per_bl
         url = 'https://deep-index.moralis.io/api/v2/block/'+str(block_on_chain)+'/nft/transfers'
         counter = 0
 
-        while lines_to_save_data_per_block > offset:
-            counter += 1
-            print('trying to get block '+ str(counter) + 'from blockchain ' + str(block_on_chain))
-            query= {'chain':chain, 'limit':limit, 'offset': offset}
-            response = requests.get(url=url, headers=my_headers, params=query)
-            response = response.json()
-            df_supp = pd.DataFrame(response['result'], dtype=str)
-            
-            if len(df_supp)==0: break
-            df = df.append(df_supp, ignore_index=True)
+        for i in range(maxTries):
+            try:
+                time.sleep(downloadTime)
+                while lines_to_save_data_per_block > offset:
+                    counter += 1
+                    # print('trying to get block '+ str(counter) + 'from blockchain ' + str(block_on_chain))
+                    query= {'chain':chain, 'limit':limit, 'offset': offset}
+                    response = requests.get(url=url, headers=my_headers, params=query)
+                    response = response.json()
+                    df_supp = pd.DataFrame(response['result'], dtype=str)
+                    
+                    if len(df_supp)==0: break
+                    df = df.append(df_supp, ignore_index=True)
 
-            if len(response['result']) == limit : offset += limit
-            else : break
-            
-            time.sleep(1)
-            df.append(df_supp, ignore_index=True)
-            
-    df.to_csv(data_folder+'moralis_blocks'+str(block_start)+'-'+str(block_on_chain)+'.csv', index=False)
+                    if len(response['result']) == limit : offset += limit
+                    else : break
+                    df.append(df_supp, ignore_index=True)
+                break
+            except Exception:
+                time.sleep(10)
+                if i == maxTries - 1 :
+                    print("Failed at block ", str(block_on_chain))
+                    df.to_csv(data_folder+'moralis_blocks'+str(block_start)+'-'+str(block_on_chain)+'.csv', index=False)
+                    print(Exception)
+                    return
+                continue
+
+    return df.to_csv(data_folder+'moralis_blocks'+str(block_start)+'-'+str(block_on_chain)+'.csv', index=False)
--- webscraper/etherscan.py
+++ webscraper/etherscan.py
@@ -8,3 +8,13 @@ def webScraperPrice(ethTransaction_hash="0xbc8412594ffa7382e8306215ece5b4e041fc6
     currentTradingValue = browser.find_element_by_xpath('//*[@id="tokenpricebutton"]').text
     browser.quit()
     return estimatedTradeValue,currentTradingValue
+
+def webScraperTokenName(ethTransaction_hash="0xFBeef911Dc5821886e1dda71586d90eD28174B7d"):
+    url = 'https://etherscan.io/token/'+ethTransaction_hash
+    browser = webdriver.Chrome()
+    browser.get(url)
+    nftSymbol = browser.find_element_by_xpath('/html/head/meta[15]').get_property('content')
+    nftName = browser.find_element_by_xpath('/html/body/div[1]/main/div[1]/div/div[1]/h1/div/span').text
+    browser.quit()
+    nftSymbol = nftSymbol[nftSymbol.find("(")+1:nftSymbol.find(")")]
+    return nftSymbol,nftName
